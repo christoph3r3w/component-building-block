71b101bb19c55964321b984243721405f439d586
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-11-19 11:36:03 +0100

    added props and snippets
    with props and snippets i can define the contexts of the window components which the children content has to adapt to

---
+++ src/lib/Window.svelte
@@ -0,0 +1,46 @@
+
+<script lang=ts>
+
+	interface Props {
+		title: string;
+		context: string;
+		role: string;
+		text:string;
+		class: string;
+		color:string;
+		children?: import('svelte').Snippet<[]>; 
+	}
+
+	let {title,context,role,text, children, class: CLASS,color, ...props} : Props = $props(); 
+
+</script>
+
+{#snippet A()}
+	{#if title}
+		<h1>{title}</h1>
+	{/if}
+	{#if context}
+		<p>{context}</p>
+	{/if}
+	{#if children}
+	<section>
+		{@render children()}
+	</section>
+	{/if}
+{/snippet}
+
+{#snippet B()}
+
+	{#if title}
+	<div class="header">
+		<h1>{title}</h1>
+		<p>{context}</p>
+	</div>
+	{/if}
+
+	{#if children}
+	<section>
+		{@render children()}
+	</section>
+	{/if}
+{/snippet}
